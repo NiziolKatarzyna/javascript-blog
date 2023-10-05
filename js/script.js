598e5adc5c068d64043c145571848dfa09954624
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-10-05 11:41:34 +0200

    Get href from cklickedElement

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ function titleClickHandler(event) {
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  event.preventDefault();
   const clickedElement = this;
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
@@ -22,7 +23,9 @@ function titleClickHandler(event) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
