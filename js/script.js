f48a0ba526233b1e70f663f8c30afbd449a7fcee
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-10-09 12:28:28 +0200

    Bug solution

--- js/script.js
+++ js/script.js
@@ -6,14 +6,11 @@ function titleClickHandler(event) {
   console.log('Link was clicked!');
 
   /* [DONE]remove class 'active' from all article links  */
-
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
   /* [DONE] add class 'active' to the clicked link */
-
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
@@ -37,12 +34,7 @@ function titleClickHandler(event) {
   console.log('clickedElement:', clickedElement);
 }
 
-const links = document.querySelectorAll('.titles a');
-
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
-}
-
+/*generate titlelist*/
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -53,7 +45,6 @@ function generateTitleLinks() {
   titleList.innerHTML = '';
 
   /* [DONE]for each article */
-
   const articles = document.querySelectorAll(optArticleSelector);
 
   let html = '';
@@ -88,3 +79,10 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
