9c97230893137780f3d5b1daad9131432712727d
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-10-10 12:48:43 +0200

    change in js get tags from  data-tags

--- js/script.js
+++ js/script.js
@@ -102,6 +102,8 @@ function generateTags() {
     /* [IN PROGRESS] make html variable with empty string */
     let html = '';
     /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log(articleTags);
 
     /* split tags into array */
     /* START LOOP: for each tag */
